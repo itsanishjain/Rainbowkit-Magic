16aa358fb7be5e5cba8165fd12eccff74f05b457
anishjain <helloanishjain@gmail.com>
2023-01-14 15:04:26 +0530

    addgoogle login

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const connectors = connectorsForWallets([
   },
 ]);
 const wagmiClient = createClient({
-  autoConnect: true,
+  autoConnect: false,
   connectors,
   provider,
 });
--- src/RainbowMagicConnector.js
+++ src/RainbowMagicConnector.js
@@ -3,13 +3,15 @@ import { MagicConnector } from "@everipedia/wagmi-magic-connector";
 export const rainbowMagicConnector = ({ chains }) => ({
   id: "magic",
   name: "Magic",
-  iconUrl: "https://alpha.everipedia.org/images/magiclink.svg",
+  iconUrl:
+    "https://images.unsplash.com/photo-1673434524408-22e6ef86a1ef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
   iconBackground: "#fff",
   createConnector: () => {
     const connector = new MagicConnector({
       chains: chains,
       options: {
-        apiKey: "YOUR_API_KEY",
+        // isDarkMode: true,
+        apiKey: "pk_live_672BC627E5CA1E68",
         oauthOptions: {
           providers: ["google", "twitter", "discord"],
         },
